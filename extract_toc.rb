d73f54c8a4ea254208fe75fdf56f7f747e81bd7b
Aaron Elkiss <aelkiss@umich.edu>
2017-12-21 17:28:52 -0500

    toc extraction (WIP)

---
+++ extract_toc.rb
@@ -0,0 +1,100 @@
+#!ruby
+
+require 'ostruct'
+require 'pry'
+
+class Dance 
+
+  def initialize
+    @names = []
+    @dancers = []
+  end
+
+  attr_reader :dancers, :names
+  attr_accessor :toc_name, :filename
+
+  def figure_out_names
+    if(m = names[0].match(/^(.*)@(.*)$/) )
+       @crossref_name = names[0]
+       @sort_name = m[1]
+       names[0] = m[2]
+    elsif(toc_name != names[0])
+      @index_sort_name = names.shift
+      @crossref_name = toc_name
+    else
+      @crossref_name = toc_name
+    end
+
+    if(toc_name != names[0])
+      raise "next remaining name #{names[0]} doesn't match the toc name #{toc_name}"
+    end
+    names.shift # first name now matches toc name
+  end
+
+  def assert_toc_name_match
+  end
+
+  def add_dance_metadata
+    figure_out_names
+
+    puts
+    puts "DANCE: #{filename}"
+    puts "% name: #{toc_name}"
+
+    names.each do |name|
+      puts "% alt_name: #{name}"
+    end
+
+    puts "% index_sort_name: #{index_sort_name}" if index_sort_name
+    puts "% sort_name: #{sort_name}" if sort_name
+
+    dancers.each do |dancers|
+      # dance formation crossref defaults to main name
+      dancers_no_default = dancers.sub(/!#{crossref_name}$/,'')
+      puts "% dancers: #{dancers_no_default}"
+    end
+
+  end
+
+
+  private
+
+  attr_reader :index_sort_name, :sort_name, :crossref_name
+end
+
+class TOCExtractor
+
+  def parse(fh)
+    dance = nil
+
+    fh.readlines.each do |line|
+      if line.match %r(\\begin{minipage})
+        dance = Dance.new
+      end
+
+      if (m = line.match %r(\\index{name}{(.*)}))
+        dance.names.push(m[1])
+      end
+
+      if (m = line.match %r(\\index{dancers}{(.*)}))
+        dance.dancers.push(m[1])
+      end
+        
+      if (m = line.match %r(\\addcontentsline{toc}{section}{(.*)}))
+        dance.toc_name = m[1]
+      end
+
+      if (m = line.match %r(\\lilypondfile{(.*)}))
+        dance.filename = m[1]
+      end
+
+      if line.match %r(\\end{minipage})
+        dance.add_dance_metadata
+      end
+
+    end
+  end
+
+end
+
+TOCExtractor.new.parse(STDIN)
